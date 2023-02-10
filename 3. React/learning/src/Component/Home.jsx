943c74c1b3fa55c41e6056ca0bd5ad34e37d3469
Sam <omprakash121samuni@gmail.com>
2023-02-10 14:52:44 +0530

    Learing Redux

--- 3. React/learning/src/Component/Home.jsx
+++ 3. React/learning/src/Component/Home.jsx
@@ -1,12 +1,18 @@
 import React from 'react'
-import WindowSizeList from './WindowSizeList'
+import Auth from './Redux/Auth'
+import Balance from './Redux/Balance'
+import Banking from './Redux/Banking'
+import AccountStatus from './Redux/AccountStatus'
 
 
 function Home() {
     return (
         <div>
             <h1>Home</h1>
-            <h2><WindowSizeList /></h2>
+            <Auth />
+            <Balance />
+            <Banking />
+            <AccountStatus />
         </div>
     )
 }
--- 3. React/learning/src/index.js
+++ 3. React/learning/src/index.js
@@ -3,9 +3,19 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 
+import { configureStore } from '@reduxjs/toolkit';
+import { Provider } from 'react-redux';
+import { rootReducer } from './Component/Reducer/combinedReducer';
+
+const store = configureStore({
+  reducer: rootReducer,
+})
+
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+    <Provider store={store}>
+      <App />
+    </Provider>
   </React.StrictMode>
 );
